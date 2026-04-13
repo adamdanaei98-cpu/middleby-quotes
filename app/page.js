0e432f5c35fc58008d21a98c3fd3b603908bd9b0
adamdanaei98-cpu <adam.danaei98@gmail.com>
2026-04-12 22:57:43 -0500

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,7 @@
+// app/page.js
+import { redirect } from 'next/navigation';
+import { getCurrentUser } from '@/lib/auth';
+export default async function Home() {
+  const user = await getCurrentUser();
+  redirect(user ? '/builder' : '/login');
+}
